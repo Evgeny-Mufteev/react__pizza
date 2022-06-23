14c2145c74fee483c53a36b56507d8b2d5db5cec
evgeny <evgenymufteev@gmail.com>
2022-06-23 22:12:31 +0300

    добавил роутинг и библиотеку скелетон

--- src/App.js
+++ src/App.js
@@ -1,45 +1,23 @@
 import React from "react";
-import "./scss/app.scss";
+import { Routes, Route } from "react-router-dom";
+
 import Header from "./components/Header";
-import Categories from "./components/Categories";
-import Sort from "./components/Sort";
-import PizzaBlock from "./components/PizzaBlock";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import NotFound from "./pages/NotFound";
 
+import "./scss/app.scss";
 const App = () => {
-  const [items, setItems] = React.useState([]);
-
-  React.useEffect(() => {
-    fetch("https://62b3687ca36f3a973d21a9fc.mockapi.io/items")
-      .then((res) => res.json())
-      .then((arrPizzas) => {
-        setItems(arrPizzas);
-      });
-  }, []);
-
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {items.map((obj) => (
-              <PizzaBlock
-                key={obj.id}
-                {...obj}
-                // либо
-                // title={obj.title}
-                // price={obj.price}
-                // imageUrl={obj.imageUrl}
-                // sizes={obj.sizes}
-                // types={obj.types}
-              />
-            ))}
-          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </div>
       </div>
     </div>
