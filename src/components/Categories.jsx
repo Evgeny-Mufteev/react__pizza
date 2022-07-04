d7e7ec16757598ecc9f53fc7f37458d00837816d
evgeny <evgenymufteev@gmail.com>
2022-07-04 21:01:20 +0300

    вынес state сортировки и категорий в родителя, настроил сортировку

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,20 +1,14 @@
 import React from "react";
 
-const Categories = () => {
-  const [activeIdex, setActiveIdex] = React.useState(0);
-
+const Categories = ({ value, onChangeCategory }) => {
   const categories = ["Все", "Мясные", "Вегетарианская", "Гриль", "Острые", "Закрытые"];
-
-  // const onClickCategory = (index) => {
-  //   setActiveIdex(index);
-  // };
 
   return (
     <div className="categories">
       <ul>
-        {categories.map((value, i) => (
-          <li key={i} onClick={() => setActiveIdex(i)} className={activeIdex === i ? "active" : ""}>
-            {value}
+        {categories.map((categoryName, i) => (
+          <li key={i} onClick={() => onChangeCategory(i)} className={value === i ? "active" : ""}>
+            {categoryName}
           </li>
         ))}
       </ul>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,21 +9,35 @@ const Home = () => {
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
 
+  const [categoryId, setCategoryId] = React.useState(0);
+  const [sortType, setSortType] = React.useState({
+    name: "популярности",
+    sortProperty: "rating",
+  });
+
   React.useEffect(() => {
-    fetch("https://62b3687ca36f3a973d21a9fc.mockapi.io/items")
+    setIsLoading(true);
+
+    const category = categoryId > 0 ? `category=${categoryId}` : "";
+    const sortBy = sortType.sortProperty.replace("-", "");
+    const order = sortType.sortProperty.includes("-") ? "asc" : "desc";
+
+    fetch(
+      `https://62b3687ca36f3a973d21a9fc.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`,
+    )
       .then((res) => res.json())
       .then((arrPizzas) => {
         setItems(arrPizzas);
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, []);
+  }, [categoryId, sortType]);
   return (
     <>
       <div className="container">
         <div className="content__top">
-          <Categories />
-          <Sort />
+          <Categories value={categoryId} onChangeCategory={(i) => setCategoryId(i)} />
+          <Sort value={sortType} onChangeSort={(i) => setSortType(i)} />
         </div>
       </div>
       <h2 className="content__title">Все пиццы</h2>
