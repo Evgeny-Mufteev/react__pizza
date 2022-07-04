60fc40bd5e17de730a295059ff31d3a6febc6a6f
evgeny <evgenymufteev@gmail.com>
2022-07-04 23:42:23 +0300

    добавил поиск и пагинацию (mock api работает фигово)))

---
+++ src/components/Search/index.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+import styles from "./search.module.scss";
+
+const Search = ({ searchValue, setSearchValue }) => {
+  return (
+    <div className={styles.root}>
+      <svg
+        className={styles.icon}
+        enable-background="new 0 0 32 32"
+        id="Editable-line"
+        version="1.1"
+        viewBox="0 0 32 32"
+        xmlns="http://www.w3.org/2000/svg">
+        <circle
+          cx="14"
+          cy="14"
+          fill="none"
+          id="XMLID_42_"
+          r="9"
+          stroke="#000000"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-miterlimit="10"
+          stroke-width="2"
+        />
+        <line
+          fill="none"
+          id="XMLID_44_"
+          stroke="#000000"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-miterlimit="10"
+          stroke-width="2"
+          x1="27"
+          x2="20.366"
+          y1="27"
+          y2="20.366"
+        />
+      </svg>
+      <input
+        value={searchValue}
+        onChange={(event) => setSearchValue(event.target.value)}
+        className={styles.input}
+        placeholder="Поиск пиццы..."
+      />
+      {searchValue && (
+        <svg
+          onClick={() => setSearchValue("")}
+          className={styles.clearIcon}
+          enable-background="new 0 0 32 32"
+          height="32px"
+          id="Layer_1"
+          version="1.1"
+          viewBox="0 0 32 32"
+          width="32px"
+          xmlns="http://www.w3.org/2000/svg">
+          <g>
+            <polyline
+              fill="none"
+              points="   649,137.999 675,137.999 675,155.999 661,155.999  "
+              stroke="#FFFFFF"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-miterlimit="10"
+              stroke-width="2"
+            />
+            <polyline
+              fill="none"
+              points="   653,155.999 649,155.999 649,141.999  "
+              stroke="#FFFFFF"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-miterlimit="10"
+              stroke-width="2"
+            />
+            <polyline
+              fill="none"
+              points="   661,156 653,162 653,156  "
+              stroke="#FFFFFF"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-miterlimit="10"
+              stroke-width="2"
+            />
+          </g>
+          <g>
+            <path d="M11.312,12.766c0.194,0.195,0.449,0.292,0.704,0.292c0.255,0,0.51-0.097,0.704-0.292c0.389-0.389,0.389-1.02,0-1.409   L4.755,3.384c-0.389-0.389-1.019-0.389-1.408,0s-0.389,1.02,0,1.409L11.312,12.766z" />
+            <path d="M17.407,16.048L28.652,4.793c0.389-0.389,0.389-1.02,0-1.409c-0.389-0.389-1.019-0.561-1.408-0.171L15.296,15   c0,0-0.296,0-0.296,0s0,0.345,0,0.345L3.2,27.303c-0.389,0.389-0.315,1.02,0.073,1.409c0.194,0.195,0.486,0.292,0.741,0.292   s0.528-0.097,0.722-0.292L15.99,17.458l11.249,11.255c0.194,0.195,0.452,0.292,0.706,0.292s0.511-0.097,0.705-0.292   c0.389-0.389,0.39-1.02,0.001-1.409L17.407,16.048z" />
+          </g>
+        </svg>
+      )}
+    </div>
+  );
+};
+export default Search;
