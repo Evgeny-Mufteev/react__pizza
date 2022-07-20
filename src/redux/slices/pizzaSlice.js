9d935ef65b77e35478e7450606cf023774721e80
evgeny <evgenymufteev@gmail.com>
2022-07-20 21:14:01 +0300

    дальше оптимизация приложения

--- src/redux/slices/pizzaSlice.js
+++ src/redux/slices/pizzaSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const fetchPizzas = createAsyncThunk("pizza/fetchPizzasStatus", async (params) => {
+export const fetchPizzas = createAsyncThunk("pizza/fetchPizzasStatus", async (params, thunkApi) => {
   const { sortBy, order, category, search, currentPage } = params;
   const { data } = await axios.get(
     `https://626d16545267c14d5677d9c2.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}&${search}`,
